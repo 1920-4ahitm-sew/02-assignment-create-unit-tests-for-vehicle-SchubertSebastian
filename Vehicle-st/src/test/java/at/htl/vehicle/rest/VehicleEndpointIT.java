eadd91e4fbb995cf29d153cb8349e3c4812a4b4f
Sebastian Schubert <39259189+C0rruptdeX@users.noreply.github.com>
2019-10-17 19:40:09 +0200

    fixed: changed https to http

--- Vehicle-st/src/test/java/at/htl/vehicle/rest/VehicleEndpointIT.java
+++ Vehicle-st/src/test/java/at/htl/vehicle/rest/VehicleEndpointIT.java
@@ -24,10 +24,10 @@ public class VehicleEndpointIT {
     @BeforeEach
     public void initClient(){
         this.client = ClientBuilder.newClient();
-        this.target = client.target("https://localhost:8080/vehicle/api/vehicle");
+        this.target = client.target("http://localhost:8080/vehicle/api/vehicle");
     }
 
-    /*
+
     @Test
     public void fetchVehicle(){
 
@@ -37,26 +37,30 @@ public class VehicleEndpointIT {
         System.out.println("payload = " + payload);
 
     }
-    */
+
 
     @Test
     public void crud() {
         Response response = this.target.request(MediaType.APPLICATION_JSON).get();
-        assertThat(response.getStatus(), is(200));
+        assertThat(response.getStatus(),is(200));
         JsonArray payload = response.readEntity(JsonArray.class);
         System.out.println("payload = " + payload);
-        assertThat(payload, not(empty()));
+        assertThat(payload,not(empty()));
 
         JsonObject vehicle = payload.getJsonObject(0);
-        assertThat(vehicle.getString("brand"), equalTo("Opel 42"));
-        assertThat(vehicle.getString("type"), equalTo("Commodore"));
+        assertThat(vehicle.getString("brand"),equalTo("Opel 42"));
+        assertThat(vehicle.getString("type"),startsWith("Commodore"));
 
-        JsonObject dedicatedVehicle = this.target.path("43").request(MediaType.APPLICATION_JSON).get(JsonObject.class);
-        assertThat(dedicatedVehicle.getString("brand"), containsString("43"));
-        assertThat(dedicatedVehicle.getString("brand"), equalTo("Opel 43"));
+        JsonObject dedicatedVehicle = this.target.path("43")
+                .request(MediaType.APPLICATION_JSON)
+                .get(JsonObject.class);
+        assertThat(dedicatedVehicle.getString("brand"),containsString("43"));
+        assertThat(dedicatedVehicle.getString("brand"),equalTo("Opel 43"));
 
-        Response deleteResponse = this.target.path("42").request(MediaType.APPLICATION_JSON).delete();
-        assertThat(deleteResponse.getStatus(), is(204));
+        Response deleteResponse = this.target.path("42")
+                .request(MediaType.APPLICATION_JSON).delete();
+        assertThat(deleteResponse.getStatus(),is(204));
+
 
 
     }
